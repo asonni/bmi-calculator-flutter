f40534dbc2bfb606dbb8ab9a59003270c81484b1
Aladdin Sonni <aladdin.sonni@gmail.com>
2020-02-20 14:51:31 +0200

    Adding the BMI Calculator Functionality

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -7,6 +7,7 @@ import '../constants.dart';
 import '../screens/results_page.dart';
 import '../components/bottom_button.dart';
 import '../components/round_icon_button.dart';
+import '../calculator_brain.dart';
 
 enum Gender {
   male,
@@ -217,10 +218,17 @@ class _InputPageState extends State<InputPage> {
           BottomButton(
             buttonTitle: 'CALCULATE',
             onTab: () {
+              CalculatorBrain calc =
+                  CalculatorBrain(height: _height, weight: _weight);
+
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => ResultsPage(),
+                  builder: (context) => ResultsPage(
+                    bmiResult: calc.calculateBMI(),
+                    resultText: calc.getResult(),
+                    interpretation: calc.getInterpretation(),
+                  ),
                 ),
               );
             },
--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -4,6 +4,16 @@ import '../components/reusable_card.dart';
 import '../components/bottom_button.dart';
 
 class ResultsPage extends StatelessWidget {
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
+  ResultsPage({
+    @required this.bmiResult,
+    @required this.resultText,
+    @required this.interpretation,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,15 +43,15 @@ class ResultsPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    'Normal',
+                    resultText.toUpperCase(),
                     style: kResultTextStyle,
                   ),
                   Text(
-                    '18.3',
+                    bmiResult,
                     style: kBMITextStyle,
                   ),
                   Text(
-                    'Your BMI result is quite low, you should eat more!',
+                    interpretation,
                     textAlign: TextAlign.center,
                     style: kBodyTextStyle,
                   ),
